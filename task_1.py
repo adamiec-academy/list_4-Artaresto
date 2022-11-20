8addf01633b9572f098d63573802ab4ca7a08a41
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-11-20 13:41:50 +0100

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,8 +1,5 @@
 def is_palindrome(text):
     x = text[::-1].lower()
     y = text.lower()
-    if y.replace(" ", "") == x.replace(" ", ""):
-        return True
-    else:
-        return False
+    return y.replace(" ", "") == x.replace(" ", "")
   
