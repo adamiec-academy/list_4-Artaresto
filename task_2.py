10f722a0c1fc44259daad919ca6878b41206f099
Artaresto <89027893+Artaresto@users.noreply.github.com>
2022-11-21 09:18:41 +0100

    Update task_2.py

--- task_2.py
+++ task_2.py
@@ -1,2 +1,13 @@
 def my_split(text):
-    return [text]
+    split=[]
+    space = ""
+    for i in text:
+        if i == " ":
+            split.append(space)
+            space = ""
+        else:
+            space += i
+        return text
+    if space:
+        split.append(space)
+        return text
